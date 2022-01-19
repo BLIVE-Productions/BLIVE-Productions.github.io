902fcb932ce8049ec4072e8f51bc14c3a3c48fe8
paultje1900 <paulboerma@live.nl>
2022-01-19 13:14:20 +0100

    :art: code cleanup scollJS

--- js/scroll.js
+++ js/scroll.js
@@ -2,22 +2,13 @@ const scrollId = document.getElementById('scroll');
 
 if(scrollId != null) {
     
-    let scrollDiv = scrollId.style;
-
-    greyBackgroundImages = document.getElementsByClassName('greyscale');
-    
+    let scrollDiv = scrollId.style;    
     window.addEventListener("scroll", () => {
         let scroll = this.scrollY;
         if(scroll > 50) { scrollDiv.opacity = 0; }
-        else { scrollDiv.opacity = 1; }
-    
-        let percentage = scroll/3;
-        greyBackgroundImages[0].style.filter = "grayscale(" + (100 - percentage) + "%)";
-        greyBackgroundImages[1].style.filter = "grayscale(" + (100 - percentage) + "%)";
-        greyBackgroundImages[2].style.filter = "grayscale(" + (100 - percentage) + "%)";
-    
+        else { scrollDiv.opacity = 1; }    
     });
-    
+
 }
 
 
