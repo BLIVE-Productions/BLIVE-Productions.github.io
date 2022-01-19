51766e7e5ff1e75095ea3879592fed275b878b98
paultje1900 <paulboerma@live.nl>
2022-01-19 16:37:08 +0100

    :iphone: cool updates to the website!

--- js/title-letters.js
+++ js/title-letters.js
@@ -1,20 +1,25 @@
-const letters = document.getElementById("title");
 const fontFamilyPool = ['Times New Roman', 'Brightness', "DarkMage", "courier new", 'EarwigFactory', 'MagnificentSerif', 'Viner Hand ITC'];
 
-if(letters != null) {
+const titleLetters = document.getElementById("title");
+const offerteLetters = document.getElementById("offerte-titel");
 
-    setLetters();
-
+if(titleLetters != null) {
+    setLettersFromDomElement(titleLetters, 100, 80);
     setInterval(() => {
-        setLetters();
+        setLettersFromDomElement(titleLetters, 100, 80);
     },1200);
-    
-    function setLetters() {
-        for(let i = 0; i < letters.children.length; i++) {
-            letters.children[i].style.fontFamily  = fontFamilyPool[Math.floor(Math.random() * 8)];
-            letters.children[i].style.fontSize  = Math.floor(Math.random() * (45 - 30)) + 30 + "px";
-        }
-    }
-    
 }
 
+if(offerteLetters != null) {
+    setLettersFromDomElement(offerteLetters, 50, 35);
+    setInterval(() => {
+        setLettersFromDomElement(offerteLetters, 50, 35);
+    },1200);
+}
+
+function setLettersFromDomElement(domElement, upperLimit, minimum) {
+    for(let i = 0; i < domElement.children.length; i++) {
+        domElement.children[i].style.fontFamily  = fontFamilyPool[Math.floor(Math.random() * 8)];
+        domElement.children[i].style.fontSize  = Math.floor(Math.random() * (upperLimit - minimum)) + minimum + "px";
+    }
+}
